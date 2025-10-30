acca526e83bc7fa0552be6932e0e0d36c5d8bf20
saffie899-creator <saffie899@gmail.com>
2025-10-30 16:07:35 +0000

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,14 @@
+ # 'demo'.py
+# from operations import
+
+print("001", "Python Basics", "John Doe", "Fiction", 5)
+print("002", "Machine Learning", "Alice Smith", "Non-Fiction", 3)
+
+print("M1", "Sheku Bangura", "sheku@example.com")
+print('M2', "Fatmata Kanu", "fatmata@example.com")
+
+print("search_book('Python")
+print("borrow_book('M1", "001")
+print("borrow_book('M1", "002")
+print("return_book('M1", "001")
+print("delete_member('M1") # should fail if borrowed book remains
